b4bf355a4285780321f54b304f299551bfa558da
Wander <wander178@hotmail.com>
2026-02-20 00:24:08 -0300

    refactor(participation-service): remove unnecessary methods
    removed the following methods: update, get all, and delete

--- src/participation/participation.controller.ts
+++ src/participation/participation.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { ParticipationService } from './participation.service';
 import { CreateParticipationDto } from './dto/create-participation.dto';
-import { UpdateParticipationDto } from './dto/update-participation.dto';
 
 @Controller('participations')
 export class ParticipationController {
@@ -12,23 +11,8 @@ export class ParticipationController {
     return this.participationService.create(createParticipationDto);
   }
 
-  @Get()
-  findAll() {
-    return this.participationService.findAll();
-  }
-
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.participationService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateParticipationDto: UpdateParticipationDto) {
-    return this.participationService.update(+id, updateParticipationDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.participationService.remove(+id);
   }
 }
--- src/participation/participation.service.ts
+++ src/participation/participation.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateParticipationDto } from './dto/create-participation.dto';
-import { UpdateParticipationDto } from './dto/update-participation.dto';
 import { Repository } from 'typeorm';
 import { Participation } from './entities/participation.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -17,29 +16,7 @@ export class ParticipationService {
     return this.repository.save(participation)
   }
 
-  findAll() {
-    return this.repository.find()
-  }
-
   findOne(id: number) {
      return this.repository.findOneBy({ id })
-  }
-
-  async update(id: number, updateParticipationDto: UpdateParticipationDto) {
-    const participation = await this.repository.findOneBy({ id })    
-    
-    if(!participation) return null
-
-    this.repository.merge(participation, updateParticipationDto)    
-
-    return this.repository.save(participation)
-  }
-
-  async remove(id: number) {
-   const participation = await this.repository.findOneBy({ id })    
-    
-    if(!participation) return null
-
-    return this.repository.remove(participation)
   }
 }
